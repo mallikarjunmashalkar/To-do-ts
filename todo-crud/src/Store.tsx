2421bfe7ce1e0cf8e504bbc040fbf84ef2b51aca
Mallikarjun <mashalkar@bdsus.net>
2024-01-25 18:05:25 +0530

    first commit

---
+++ todo-crud/src/Store.tsx
@@ -0,0 +1,9 @@
+interface User {
+  item: string;
+  id: number;
+}
+
+export const userList: User[] = [
+  { item: "Mango", id: 1 },
+  { item: "Apple", id: 2 },
+];
---
+++ todo-crud/src/main.tsx
@@ -0,0 +1,23 @@
+import React, { useReducer } from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import userReduser from "./userReduser.tsx";
+
+const rootReducer = {
+  users: userReduser,
+};
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
